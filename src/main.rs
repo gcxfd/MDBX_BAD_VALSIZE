622b91c8556dd5661aebc6e5b1f7974129f4f668
gcxfd <i@rmw.link>
2021-11-13 09:27:33 +0800

    🔶

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use blake3::guts::{parent_cv, ChunkState, CHUNK_LEN};
 
-fn main() {
+fn test1() {
   let mut hasher = blake3::Hasher::new();
   let mut buf = [0; CHUNK_LEN];
 
@@ -31,4 +31,37 @@ fn main() {
   println!("{}", hasher.finalize());
   println!("{}", root);
   println!("{}", root == hasher.finalize());
+}
+
+fn test2() {
+  let mut hasher = blake3::Hasher::new();
+  let mut buf = [0; CHUNK_LEN];
+
+  buf[0] = 'a' as u8;
+  hasher.update(&buf);
+  let chunk0_cv = ChunkState::new(0).update(&buf).finalize(false);
+
+  buf[0] = 'b' as u8;
+  hasher.update(&buf);
+  let chunk1_cv = ChunkState::new(1).update(&buf).finalize(false);
+
+  buf[0] = 'c' as u8;
+  hasher.update(&buf);
+  let chunk2_cv = ChunkState::new(2).update(&buf).finalize(false);
+
+  hasher.update(b".");
+  let end = ChunkState::new(3).update(b".").finalize(false);
+
+  let parent1 = parent_cv(&chunk0_cv, &chunk1_cv, false);
+  let parent2 = parent_cv(&chunk2_cv, &end, false);
+  let root = parent_cv(&parent1, &parent2, true);
+
+  println!("{}", hasher.finalize());
+  println!("{}", root);
+  println!("{}", root == hasher.finalize());
+}
+
+fn main() {
+  test1();
+  test2();
 }
